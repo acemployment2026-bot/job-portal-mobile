9e24bdadfd0306b12ad2920e5619bfc765ee2e08
Abdul Realmrook <abdulrealmrook@gmail.com>
2026-06-11 21:16:46 +0530

    Update API URL to new Render backend

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 // const API_BASE_URL = 'http://192.168.1.51:5001/api'; // Use this for Physical Devices (your machine's local IP)
 // const API_BASE_URL = 'http://10.0.2.2:5001/api'; // Use this if you are testing on an Android Emulator
 // const API_BASE_URL = 'http://10.102.75.130:8081/'; // Use this for local network device
-const API_BASE_URL = 'https://jobsearchindia.net/api'; // Production server
+const API_BASE_URL = 'https://job-portal-backend-rij5.onrender.com/api'; // Production server
 
 export default {
     API_BASE_URL,
